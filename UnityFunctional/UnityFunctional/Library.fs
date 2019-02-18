d10f576f747c62f1d7124f880f42b89fc58773ba
Tobias Morell <tobiasmorell@hotmail.com>
2019-02-18 10:40:06 +0100

    Add source files

---
+++ UnityFunctional/UnityFunctional/Library.fs
@@ -0,0 +1,17 @@
+namespace UnityFunctional
+open UnityEngine;
+open System;
+
+
+type TestComponent() = 
+    inherit UnityEngine.MonoBehaviour()
+    [<SerializeField>]
+    let mutable speed:float32 = 2.0f
+    [<SerializeField>]
+    let mutable height = 25.0f
+
+    member this.Start() =
+        this.transform.localScale <- this.transform.localScale + new Vector3(height, 0.0f);
+
+    member this.Update() =
+        this.transform.Rotate(Vector3.down, speed * Time.deltaTime);
