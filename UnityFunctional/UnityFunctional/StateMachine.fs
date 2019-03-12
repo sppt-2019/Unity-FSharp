ec9aac60b2c8e995c06f3cc657209ab9698b5fd9
Tobias Morell <tobiasmorell@hotmail.com>
2019-03-12 10:49:47 +0100

    Add RTS AI task

---
+++ UnityFunctional/UnityFunctional/StateMachine.fs
@@ -0,0 +1,11 @@
+﻿namespace UnityFunctional
+open UnityEngine
+open System
+
+type State = Fleeing | Moving | Shooting
+
+type FRP_StateMachine() =
+    inherit MonoBehaviour()
+
+    member this.JoinState(gameObject:GameObject, state:State) = 
+        Debug.Log(gameObject.name + " joined state " + state.ToString());
