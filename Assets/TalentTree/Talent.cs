fb0f5de5ed1965e2bd02fa9502977c703f898fbc
Tobias Morell <tobiasmorell@hotmail.com>
2019-03-13 13:28:49 +0100

    Almost wrapup implementation of Talents

---
+++ Assets/TalentTree/Talent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Talent", menuName = "Talent", order = 1)]
+public class Talent : ScriptableObject
+{
+    public bool Picked;
+
+    public int Strength;
+    public int Intellect;
+    public int Agility;
+
+    public Sprite Sprite;
+
+    public Talent[] SubTalents;
+}
