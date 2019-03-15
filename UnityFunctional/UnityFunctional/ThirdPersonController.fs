7ca7f220c5c98839565e252a03ad7a271c2ed9dd
Tobias Morell <tobiasmorell@hotmail.com>
2019-03-15 11:12:34 +0100

    WIP: Add preliminary FRPBehaviour

--- UnityFunctional/UnityFunctional/ThirdPersonController.fs
+++ UnityFunctional/UnityFunctional/ThirdPersonController.fs
@@ -3,4 +3,18 @@ open UnityEngine
 open System
 
 type FRP_ThirdPersonController() =
-    inherit MonoBehaviour()
+    inherit FRPBehaviour()
+
+    member this.Start() =
+        this.ReactTo (
+            FRPEvent.Keyboard, 
+            (fun a -> Input.GetKeyDown(KeyCode.Space)),
+            (fun s -> 
+                Debug.Log("FRP message: " + s)))
+
+        this.ReactTo (
+            FRPEvent.MouseClick,
+            (fun a -> Input.GetMouseButtonDown(0)),
+            (fun s ->
+                Debug.Log("FRP message: " + s)
+        ))
