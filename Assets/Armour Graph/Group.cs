7966f21b42f9c0f76044023d1ebda4572f604a0d
Tobias Morell <tobiasmorell@hotmail.com>
2019-03-20 08:34:40 +0100

    Remove merge conflict

--- Assets/Armour Graph/Group.cs
+++ Assets/Armour Graph/Group.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 
 public class Group : ScriptableObject
 {
-<<<<<<< HEAD
     public Group(ItemGroup group, IEnumerable<Item> itemsInGroup)
     {
         GroupName = group;
@@ -13,26 +12,13 @@ public class Group : ScriptableObject
     }
 
     public ItemGroup GroupName { get; set; }
-=======
-    public Group(ItemGroup itemGroup, IEnumerable<Item> itemsInGroup)
-    {
-        IemmGroup = itemGroup;
-        Items = itemsInGroup.ToList();
-    }
-
-    public ItemGroup IemmGroup { get; set; }
->>>>>>> f99be3c8e09ae87a556e98535d01e12db80e5a20
 
     public List<Item> Items { get; set; }
 
     public override string ToString()
     {
         var itms = "\n" + string.Join("\n", Items);
-
-<<<<<<< HEAD
+        
         return $"{GroupName.ToString()}:[" + itms + "]";
-=======
-        return $"{IemmGroup.ToString()}" + itms + "]";
->>>>>>> f99be3c8e09ae87a556e98535d01e12db80e5a20
     }
 }
