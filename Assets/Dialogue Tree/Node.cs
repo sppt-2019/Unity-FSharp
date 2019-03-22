2b798c74eb10308a4bef67c1b5fa936480997fbc
tgm <tgmmail@protonmail.com>
2019-03-22 09:45:30 +0100

    Add reaction enumerator

--- Assets/Dialogue Tree/Node.cs
+++ Assets/Dialogue Tree/Node.cs
@@ -9,6 +9,14 @@ public class Node
         Name = name;
         Line = line;
         ChildNames = new List<string>();
+        Reaction = NPCReaction.NoReaction;
+    }
+    public Node(string name, string line, NPCReaction reaction)
+    {
+        Name = name;
+        Line = line;
+        ChildNames = new List<string>();
+        Reaction = reaction;
     }
     public Node(string name, string line, string child)
     {
@@ -16,14 +24,31 @@ public class Node
         Line = line;
         ChildNames = new List<string>();
         ChildNames.Add(child);
+        Reaction = NPCReaction.NoReaction;
     }
     public Node(string name, string line, List<string> children)
     {
         Name = name;
         Line = line;
         ChildNames = ChildNames;
+        Reaction = NPCReaction.NoReaction;
+    }
+    public Node(string name, string line, List<string> children, NPCReaction reaction)
+    {
+        Name = name;
+        Line = line;
+        ChildNames = ChildNames;
+        Reaction = reaction;
     }
 
+    public enum NPCReaction
+    {
+        NoReaction = 0,
+        Hostile,
+        Gift,
+        Companion
+    }
+    
     public static List<Node> GetTree()
     {
         return new List<Node>
@@ -38,12 +63,12 @@ public class Node
             new Node("found", "MIN LILLE HUND! Den har været væk i flere dage. Tusind tak!", "gift"),
             new Node("move", "Her i byen taler vi pænt til hinanden!",
                 new List<string> {"die", "sorry"}),
-            new Node("die", "VI BLIVER ANGREBET!", "hostile"),
+            new Node("die", "VI BLIVER ANGREBET!", "hostile", NPCReaction.Hostile),
             new Node("sorry", "Det er i orden. Det kan ske for alle", "work"),
             new Node("goodbye", "Held og lykke på dine rejser", "no-reaction"),
-            new Node("companion", "Karakteren bliver ens følgesvend"),
-            new Node("hostile", "Karakteren bliver fjendtlig"),
-            new Node("gift", "Karakteren giver spilleren en gave"),
+            new Node("companion", "Karakteren bliver ens følgesvend", NPCReaction.Companion),
+            new Node("hostile", "Karakteren bliver fjendtlig", NPCReaction.Hostile),
+            new Node("gift", "Karakteren giver spilleren en gave", NPCReaction.Gift),
             new Node("no-reaction", "Ingen reaktion")
         };
     }
@@ -51,4 +76,6 @@ public class Node
     public string Name { get; }
     public string Line { get; }
     public List<string> ChildNames { get; set; }
+    
+    public NPCReaction Reaction { get; set; }
 }
