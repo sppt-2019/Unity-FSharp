f002932bc4d307b07c6abc64a76a732c53e0d757
Tobias Morell <tobiasmorell@hotmail.com>
2019-03-22 07:51:01 +0100

    Update test setup

--- Assets/Dialogue Tree/NPC.cs
+++ Assets/Dialogue Tree/NPC.cs
@@ -4,46 +4,32 @@ using UnityEngine;
 
 public class NPC : MonoBehaviour
 {
-    Node currentNode;
-    List<Node> nodes = new List<Node>();
-
-    public void say(Node node)
+    Node _currentNode;
+    public UnityEngine.UI.Text outText;
+    private List<Node> _nodes;
+    
+    public void Say(Node node)
     {
-        currentNode = node;
+        _currentNode = node;
         outText.text = node.Line;
     }
     
-    public void say( )
+    public void Say( )
     {
-        outText.text = currentNode.Line;
+        outText.text = _currentNode.Line;
     }
 
-    public UnityEngine.UI.Text outText;
-    
-    
     // Start is called before the first frame update
     void Start()
     {
-        nodes.Add(new Node("welcome", "Velkommen til vores lille by, hvad kan jeg hjælpe med?", "work"));
-        nodes.Add(new Node("work", "Min hund er blevet væk og jeg skulle alligevel ud og lede efter den. Har du brug for hjælp?", 
-            new List<string>{ "yes", "no"}));
-        nodes.Add(new Node("yes", "Fantastisk, mit sværd er dit!", "companion"));
-        nodes.Add(new Node("no", "Ærgeligt, sig til hvis du ombestemmer dig.", "work"));
-        nodes.Add(new Node("found", "MIN LILLE HUND! Den har været væk i flere dage. Tusind tak!", "gift"));
-        nodes.Add(new Node("move", "Her i byen taler vi pænt til hinanden!", 
-            new List<string>{ "die", "sorry"}));
-        nodes.Add(new Node("die", "VI BLIVER ANGREBET!", "hostile"));
-        nodes.Add(new Node("sorry", "Det er i orden. Det kan ske for alle", "work"));
-        nodes.Add(new Node("goodbye", "Held og lykke på dine rejser", "no"));
+        _nodes = Node.GetTree();
+        _currentNode = _nodes[0];
+        var nodeRoot = Solution1(_nodes[0], 10);
 
-        currentNode = nodes[0];
-        say();
+        Say(nodeRoot);
 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        var traces = Solution2(nodeRoot);
+        Debug.Log(traces);
+        Debug.Log(traces.Count);
     }
 }
--- Assets/Dialogue Tree/Node.cs
+++ Assets/Dialogue Tree/Node.cs
@@ -2,28 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Node : ScriptableObject
+public class Node
 {
     public Node(string name, string line)
     {
         Name = name;
         Line = line;
-        Children = new List<string>();
+        ChildNames = new List<string>();
     }
     public Node(string name, string line, string child)
     {
         Name = name;
         Line = line;
-        Children = new List<string>();
-        Children.Add(child);
+        ChildNames = new List<string>();
+        ChildNames.Add(child);
     }
     public Node(string name, string line, List<string> children)
     {
         Name = name;
         Line = line;
-        Children = Children;
+        ChildNames = ChildNames;
     }
+
+    public static List<Node> GetTree()
+    {
+        return new List<Node>
+        {
+            new Node("welcome", "Velkommen til vores lille by, hvad kan jeg hjælpe med?",
+                new List<string> {"work", "found", "move"}),
+            new Node("work",
+                "Min hund er blevet væk og jeg skulle alligevel ud og lede efter den. Har du brug for hjælp?",
+                new List<string> {"yes", "no"}),
+            new Node("yes", "Fantastisk, mit sværd er dit!", "companion"),
+            new Node("no", "Ærgeligt, sig til hvis du ombestemmer dig.", "work"),
+            new Node("found", "MIN LILLE HUND! Den har været væk i flere dage. Tusind tak!", "gift"),
+            new Node("move", "Her i byen taler vi pænt til hinanden!",
+                new List<string> {"die", "sorry"}),
+            new Node("die", "VI BLIVER ANGREBET!", "hostile"),
+            new Node("sorry", "Det er i orden. Det kan ske for alle", "work"),
+            new Node("goodbye", "Held og lykke på dine rejser", "no-reaction"),
+            new Node("companion", "Karakteren bliver ens følgesvend"),
+            new Node("hostile", "Karakteren bliver fjendtlig"),
+            new Node("gift", "Karakteren giver spilleren en gave"),
+            new Node("no-reaction", "Ingen reaktion")
+        };
+    }
+    
     public string Name { get; }
     public string Line { get; }
-    public List<string> Children { get; set; }
+    public List<string> ChildNames { get; set; }
 }
