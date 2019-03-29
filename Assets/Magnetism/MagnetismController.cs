127d9b88cff63cc02a0b0bb9ce3f6069324e83bf
tgm <tgmmail@protonmail.com>
2019-03-29 11:27:29 +0100

    Implementation of magnatism in C#

--- Assets/Magnetism/MagnetismController.cs
+++ Assets/Magnetism/MagnetismController.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class MagnetismController : MonoBehaviour
 {
+    [SerializeField]
+    public float moveSpeed;
+    private GameObject[] balls;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        balls = GameObject.FindGameObjectsWithTag("Magnetic");
+        foreach (var ball in balls)
+        {
+            ball.transform.LookAt(CalcCenter(balls));
+        }
     }
 
+    private Vector3 CalcCenter(GameObject[] objects)
+    {
+        var vec = new Vector3();
+
+        foreach (var obj in objects)
+        {
+            vec += obj.transform.position;
+        }
+
+        return new Vector3(vec.x / objects.Length,-1.51f,vec.z / objects.Length);
+    }
+    
     // Update is called once per frame
     void Update()
     {
-        
+        var center = CalcCenter(balls);
+        foreach (var ball in balls)
+        {
+            ball.transform.LookAt(center);
+            var rb = ball.GetComponent<Rigidbody>();
+            rb.MovePosition(rb.transform.position + transform.forward * moveSpeed * Time.deltaTime);
+            //ball.transform.Translate(ball.transform.forward * moveSpeed * Time.deltaTime, Space.Self);
+        }
     }
 }
