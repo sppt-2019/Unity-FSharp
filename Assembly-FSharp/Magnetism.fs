03f8ee8a77a16619487c9b48510cc31411d83932
Tobias Morell <tobiasmorell@hotmail.com>
2019-04-10 09:19:32 +0200

    Test with participant 2

--- Assembly-FSharp/Magnetism.fs
+++ Assembly-FSharp/Magnetism.fs
@@ -5,7 +5,31 @@ open System
 type FRP_MagnetismController() = 
     inherit FRPBehaviour()
 
+    [<SerializeField>]
+    let mutable speed = 5.0f
+
+    let moveMagneticBalls (obj:GameObject) (center:GameObject) = async{
+        obj.transform.LookAt(center.transform)
+        obj.transform.Translate(obj.transform.forward * Time.deltaTime * speed)
+        ()
+        }
+
+    let tmp (objs:GameObject[]) (center:GameObject) =
+        objs
+        |> Array.map (fun i -> moveMagneticBalls i center)
+        |> Async.Parallel
+        |> Async.RunSynchronously
+        ()
+
     member this.Start() = 
+        let center = GameObject.Instantiate<GameObject>(new GameObject())
         let Balls = GameObject.FindGameObjectsWithTag("Magnetic")
         Debug.Log("There are " + Balls.Length.ToString() + " magnetic balls")
+        this.ReactTo (
+            Update,
+            (fun () -> tmp Balls center ))
+
+    
+
+
 
--- Assets/AI/StateMachine.cs
+++ Assets/AI/StateMachine.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Linq;
 using UnityEngine;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 enum State
 {
@@ -19,16 +21,99 @@ class StateMachine : MonoBehaviour
     public StateMaterial[] StateMaterials;
     public GameObject ShotPrefab;
 
+    private List<Shooter> fleeingShooters = new List<Shooter>();
+    private List<Shooter> movingShooters = new List<Shooter>();
+    private List<Shooter> attackingShooters = new List<Shooter>();
+
     /*JoinState is called by the shooters to indicate that they want to be part of the state machine*/
     public void JoinState(Shooter shooter, State state)
     {
         shooter.GetComponent<Renderer>().material = StateMaterials.First(m => m.State == state).Material;
+
+        switch(state)
+        {
+            case State.Fleeing:
+                fleeingShooters.Add(shooter);
+                break;
+
+            case State.Moving:
+                movingShooters.Add(shooter);
+                break;
+
+            case State.Attacking:
+                attackingShooters.Add(shooter);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    private void Update()
+    {
+        List<Task> fleeTask = new List<Task>();
+        foreach(var fleeingShooter in fleeingShooters)
+        {
+            fleeTask.Add(Task.Run(() => Flee(fleeingShooter)));
+        }
+
+        List<Task> moveTask = new List<Task>();
+        foreach (var movingShooter in movingShooters)
+        {
+            moveTask.Add(Task.Run(() => Move(movingShooter)));
+        }
+
+        List<Task> attackTask = new List<Task>();
+        foreach (var attackingShooter in attackingShooters)
+        {
+            attackTask.Add(Task.Run(() => Attack(attackingShooter)));
+        }
+
+        Task.WhenAll(fleeTask);
+        Task.WhenAll(moveTask);
+        Task.WhenAll(attackTask);
     }
 
     /*TransferState is called by the shooters to indicate that they want to move from one state to another*/
     public void TransferState(Shooter shooter, State state)
     {
         shooter.GetComponent<Renderer>().material = StateMaterials.First(m => m.State == state).Material;
+
+        switch (state)
+        {
+            case State.Fleeing:
+                fleeingShooters.Add(shooter);
+                RemoveFromList(shooter, ref movingShooters, ref attackingShooters);
+                break;
+
+            case State.Moving:
+                movingShooters.Add(shooter);
+                RemoveFromList(shooter, ref fleeingShooters, ref attackingShooters);
+                break;
+
+            case State.Attacking:
+                attackingShooters.Add(shooter);
+                RemoveFromList(shooter, ref movingShooters, ref fleeingShooters);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    private void RemoveFromList(Shooter shooter, ref List<Shooter> list1, ref List<Shooter> list2)
+    {
+        if (list1.IndexOf(shooter) != -1)
+        {
+            list1.Remove(shooter);
+            return;
+        }
+
+        if(list2.IndexOf(shooter) != -1)
+        {
+            list2.Remove(shooter);
+            return;
+        }
     }
 
     #region State Logic
