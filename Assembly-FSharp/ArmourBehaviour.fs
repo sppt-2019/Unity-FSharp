48402e3b3886177f75ae85172fdfb0072b200726
Tobias Morell <tobiasmorell@hotmail.com>
2019-04-10 11:54:09 +0200

    Test participant 3

--- Assembly-FSharp/ArmourBehaviour.fs
+++ Assembly-FSharp/ArmourBehaviour.fs
@@ -6,8 +6,44 @@ type FRP_ArmourBehaviour() =
     inherit FRPBehaviour()
 
     member this.Start() =
-        let i = ItemStore.AllItems()
-        Debug.Log(String.Join("\n", i))
+        let allItems = ItemStore.AllItems()
+        Debug.Log(String.Join("\n", allItems))
 
-        let g = ItemStore.GroupedItems()
-        Debug.Log(String.Join("\n", g))
+        let groupedItems = ItemStore.GroupedItems()
+        Debug.Log(String.Join("\n", groupedItems))
+
+        let agilityTotal = ItemStore.AllItems() |> Array.map(fun item -> item.Agility) |> Array.sum
+        Debug.Log(agilityTotal.ToString())
+
+        let intTotal = ItemStore.AllItems() |> Array.map(fun item -> item.Intellect) |> Array.sum
+        Debug.Log(intTotal.ToString())
+
+        let strengthTotal = ItemStore.AllItems() |> Array.map(fun item -> item.Strength) |> Array.sum
+        Debug.Log(strengthTotal.ToString())
+
+        let total (items:Item[]) (getter:Item->int) =
+            items |> Array.map(fun item -> getter(item)) |> Array.sum
+
+        Debug.Log(total allItems (fun item -> item.Agility))
+               
+        let agiMod = (List.ofSeq groupedItems.[0].Items) |> List.map(fun item -> item.AgilityMod) |> List.sum
+        
+        Debug.Log(agiMod.ToString())
+
+        let getGroupMod (items:Item list) (getter:Item->float32) =
+            items |> List.map(fun item -> getter(item)) |> List.sum
+
+        let agiMod = getGroupMod (List.ofSeq groupedItems.[0].Items) (fun item -> item.AgilityMod)
+        let agiTotal = total (Array.ofSeq groupedItems.[0].Items) (fun item -> item.Agility)
+        let agiTotalWithMod = agiMod * (float32) agiTotal
+
+        Debug.Log(agiTotalWithMod.ToString())
+
+        let getTotalWithMod (items:Item list) (attribute:Item->int) (attributeMod:Item->float32) =
+            let modifier = getGroupMod items attributeMod
+            let attributeTotal = total (Array.ofSeq items) attribute
+            modifier * (float32) attributeTotal
+
+        Debug.Log((getTotalWithMod (List.ofSeq groupedItems.[0].Items) (fun item -> item.Agility) (fun item -> item.AgilityMod)).ToString())
+
+        Debug.Log("Last")
--- Assets/TalentTree/TalentTree.cs
+++ Assets/TalentTree/TalentTree.cs
@@ -1,11 +1,37 @@
 ﻿using UnityEngine;
+using System.Linq;
+using System.Collections.Generic;
 
 public class TalentTree : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        var rootTalent = Talent.TalentTreeRoot;
+
+        var pickedTalents = GetSubTalents(rootTalent, true);
+
+
+
+        Debug.Log($"Agility {pickedTalents.Sum(talent => talent.Agility)}");
+    }
+
+    private List<Talent> GetSubTalents(Talent talent, bool onlyIfPicked, bool first = true)
+    {
+        var subTalents = new List<Talent>();
+        if (first)
+        {
+            subTalents.Add(talent);
+        }
+
+        foreach (var subTalent in talent.SubTalents)
+        {
+            if (subTalent.Picked)
+            {
+                subTalents.AddRange(GetSubTalents(talent, onlyIfPicked, false));
+            }
+        }
+        return subTalents;
     }
 
     // Update is called once per frame
