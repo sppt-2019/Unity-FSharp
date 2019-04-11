606572e75682666dcb06ab328a58f3c2410adfa9
Tobias Morell <tobiasmorell@hotmail.com>
2019-04-11 08:00:23 +0200

    Add missing Talents class

---
+++ Assets/TalentTree/MyTalent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyTalent
+{
+    public int Strength;
+    public int Agility;
+    public int Intelligence;
+
+    public bool Picket;
+
+    public List<MyTalent> SubTalents = new List<MyTalent>();
+}
