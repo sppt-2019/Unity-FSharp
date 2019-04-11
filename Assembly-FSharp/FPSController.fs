0cce94c3dbf65f8fcbf370bd6df62894257facfc
Tobias Morell <tobiasmorell@hotmail.com>
2019-04-11 11:34:56 +0200

    Test with participant 4

--- Assembly-FSharp/FPSController.fs
+++ Assembly-FSharp/FPSController.fs
@@ -5,6 +5,41 @@ open System
 type FRP_FPSController() =
     inherit FRPBehaviour()
 
+    [<SerializeField>]
+    let mutable Speed = 200.0f
+
+    [<SerializeField>]
+    let mutable JumpPower = 200.0f
+
+    [<SerializeField>]
+    let mutable MouseYSensitivity = 1.0f
+
+    [<SerializeField>]
+    let mutable MouseXSensitivity = 1.0f
+
+    let mutable MovementVector = new Vector3()
+
+
     member this.Start() =
         Debug.Log("Hello from F#")
+        this.ReactTo (
+            FRPEvent.Keyboard,                            //Event type
+            (fun () -> Input.GetKeyDown(KeyCode.W)),  //Filtrering
+            (fun () -> 
+            MovementVector <- Vector3.forward*Speed
+            this.GetComponent<Rigidbody>().velocity <- MovementVector 
+            )
+            )               //Handler
 
+        this.ReactTo<float32*float32> (
+                FRPEvent.MouseMove,
+                (fun m ->
+                let (x, y) = m.Deconstruct()
+                this.transform.Rotate(x,y,0.0f)         
+                )
+        
+        )
+        
+        
+
+   
--- Assets/3rd Person Controller/ThirdPersonController.cs
+++ Assets/3rd Person Controller/ThirdPersonController.cs
@@ -2,15 +2,49 @@
 
 public class ThirdPersonController : MonoBehaviour
 {
+
+    [SerializeField]
+    private float Speed;
+
+    [SerializeField]
+    private float JumpPower;
+
+    [SerializeField]
+    private Transform _camera;
+
+    private Vector3 _movement;
+    private Vector3 _mouseRot;
+    private Rigidbody _rb;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        _rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        _movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        _movement *= Speed;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _movement.y = JumpPower;
+        }
+
+      
+
+        _rb.AddForce((transform.forward*_movement.z)+(transform.right*_movement.x));
+
+        transform.Rotate(Vector3.up, Input.GetAxis("Mouse X"));
+        _camera.transform.RotateAround(transform.position,_camera.right, Input.GetAxis("Mouse Y"));
+
+
+
+
+
+
     }
 }
