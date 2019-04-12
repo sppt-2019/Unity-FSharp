8848d54473108b744b9cfe4936552766c06ac558
Tobias Morell <tobiasmorell@hotmail.com>
2019-04-12 12:18:55 +0200

    Test with participant 6

--- Assembly-FSharp/DialogueTree.fs
+++ Assembly-FSharp/DialogueTree.fs
@@ -1,10 +1,32 @@
 namespace UnityFunctional
 open UnityEngine
 open UnityEngine.UI
+
+type DNode(dial:string,left:DNode,right:DNode) =
+    let dialogue : string = dial
+    let left  : DNode = left
+    let right : DNode = right
     
+   
 type FRP_DialogueTree() =
     inherit FRPBehaviour()
     
+    let mutable treeRoot : DNode = DNode("",,)
+        
+    let NogetAndet (dia:Node, name:string) = 
+        if dia.ChildNames.Count > 0 then
+            DNode(dia.Name, NogetAndet(dia)
+        else
+            [dia]
+
     member this.Start() =
         let dialogOptions = Node.GetExampleDialog()
+        treeRoot <- this.Parse(dialogOptions)
         ()
+
+    member this.Parse (dialogue:Node list) =
+        let child = List.find (fun l -> dialogue.[0].ChildNames.[0] == l.Name) dialogue
+        DNode(dialogue.[0].Name, [DNode(child.Name, [])])
+
+    
+    
--- Assets/Currency/Resources.cs
+++ Assets/Currency/Resources.cs
@@ -11,6 +11,25 @@ public class Resources : MonoBehaviour
     void Start()
     {
         ResourceController.UpdateResources(DildSild, MildAbild, Vildild);
-        ResourceController.UpdateResults(0, 0, 0);
+        int sild, abild, ild;
+        sild = DildSild % 17;
+        abild = ((DildSild / 17) + MildAbild) % 8;
+        ild = (((DildSild / 17) + MildAbild) / 8) + Vildild;
+        ResourceController.UpdateResults(sild, abild, ild);
+
+        var cb = CanBuy(1, 0, 0);
+        Debug.Log(cb);
+        var cnb = CanBuy(0, 0, 100);
+        Debug.Log(cnb);
+    }
+
+    bool CanBuy(int csild, int cabild, int cild)
+    {
+        int allHSild = DildSild + (((Vildild * 8) + MildAbild) * 17);
+        int allCSild = csild + (((cabild * 8) + cild) * 17);
+
+        if (allCSild > allHSild)
+            return false;
+        return true;
     }
 }
