66eecb9fba5b7e2c77ac28a780e88e2c224b7e73
Tobias Morell <tobiasmorell@hotmail.com>
2019-06-14 06:13:25 +0200

    Add benchmark scaffolding

---
+++ Assets/FrameTimer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.Serialization;
+using Debug = UnityEngine.Debug;
+
+public class FrameTimer : MonoBehaviour
+{
+    public string Folder = "results";
+    public string outputFile = "results.csv";
+    public int FrameCountCap = 10800;
+    
+    private StreamWriter _resultsFile;
+    private Stopwatch _stopwatch;
+    private int _frameCount;
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _resultsFile = new StreamWriter(Folder + outputFile, false);
+        _resultsFile.WriteLine("Frame Count,Frame Time (ns)");
+        _stopwatch = Stopwatch.StartNew();
+        Debug.Log(Stopwatch.Frequency);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        _stopwatch.Stop();
+        var time = _stopwatch.ElapsedTicks;
+        
+        if (_frameCount == FrameCountCap)
+        {
+            _resultsFile.Flush();
+            _resultsFile.Close();
+            
+            Debug.Log("Benchmarking done, you may exit the game!");
+            Application.Quit();
+        }
+        else if (_frameCount < FrameCountCap)
+        {
+            _resultsFile.WriteLine($"{_frameCount},{time}");
+        }
+        _frameCount++;
+        
+        _stopwatch.Restart();
+    }
+}
---
+++ Assets/ResultsConcatter.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class ResultsConcatter : MonoBehaviour
+{
+    public string FolderName = "il2cpp-results";
+    public string OutputFile = "output.csv";
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        File.Delete(Path.Combine(FolderName, OutputFile));
+        
+        var files = Directory.GetFiles(FolderName);
+        var concattedFiles = new List<(string key, List<string> values)>();
+        concattedFiles.Add(("Frame No.", new List<string>()));
+
+        foreach (var file in files)
+        {
+            var fileSplit = file.Split('.');
+            Debug.Log(fileSplit[0]);
+            var colName = fileSplit[0].Replace(FolderName + @"\", "");
+            Debug.Log(colName);
+            
+            concattedFiles[0].values.Add(colName);
+            var readings = ParseCsv(file);
+
+            for (var i = 0; i < readings.Count; i++)
+            {
+                if (i + 1 >= concattedFiles.Count)
+                {
+                    concattedFiles.Add((readings[i].measurement, new List<string>(){readings[i].frameTime}));
+                }
+                else
+                {
+                    concattedFiles[i+1].values.Add(readings[i].frameTime);
+                }
+            }
+        }
+
+        OutputCsv(concattedFiles);
+        Debug.Log("Nice. The files were concatted.");
+    }
+
+    private List<(string measurement, string frameTime)> ParseCsv(string file)
+    {
+        var results = new List<(string, string)>();
+        using (var parser = new StreamReader(file))
+        {
+            //Throw away the first line
+            parser.ReadLine();
+            //And read the rest of the lines into the dictionary
+            while (!parser.EndOfStream)
+            {
+                var line = parser.ReadLine();
+                var split = line.Split(',');
+                var rowName = split[0];
+                
+                results.Add((rowName, split[1]));
+            }
+        }
+
+        return results;
+    }
+
+    private void OutputCsv(List<(string key, List<string> values)> readings)
+    {
+        using (var file = new StreamWriter(Path.Combine(FolderName, OutputFile)))
+        {
+            foreach (var reading in readings)
+            {
+                var vals = string.Join(",", reading.values);
+                var o = $"{reading.key},{vals}";
+                file.WriteLine(o);
+            }
+        }
+    }
+}
